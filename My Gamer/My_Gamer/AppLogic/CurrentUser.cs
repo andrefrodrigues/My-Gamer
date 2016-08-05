c2d57d2b7a6c9b5016d844c8e43dab371a93f333
andrefrodrigues <andrefrodrigues@gmail.com>
2016-08-05 20:47:16 +0100

    started business logic

---
+++ My Gamer/My_Gamer/AppLogic/CurrentUser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Gamer.AppLogic
+{
+    class CurrentUser
+    {
+        private string _Email;
+        private string _Nome;
+        private char _Sexo;
+        public string Email {
+            get
+            {
+                return _Email;
+            }
+            set
+            {
+                _Email = value;
+            }
+        }
+        public string Nome
+        {
+            get
+            {
+                return _Nome;
+            }
+            set
+            {
+                _Nome = value;
+            }
+        }
+        public char Sexo
+        {
+            get
+            {
+                return _Sexo;
+            }
+            set
+            {
+                _Sexo = value;
+            }
+        }
+        public CurrentUser(string email,string nome,char sexo)
+        {
+            _Email = email;
+            _Nome = nome;
+            _Sexo = sexo;
+        }
+
+    }
+}
---
+++ My Gamer/My_Gamer/AppLogic/MyGamer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Gamer.AppLogic
+{
+    //this is the upper level class
+    class MyGamer
+    {
+        public CurrentUser User
+        {
+            get
+            {
+                return _User;
+            }
+            set
+            {
+                _User = value;
+            }
+        }
+        private CurrentUser _User;
+        public MyGamer()
+        {
+
+        }
+        public async void register(string email, string nome, char sexo, string password)
+        {
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                {
+                    { "email",email },
+                    {"nome",nome },
+                    { "sexo" , sexo.ToString() },
+                    {"password",password }
+                };
+                var content = new FormUrlEncodedContent(values);
+                var response = await client.PostAsync("", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
